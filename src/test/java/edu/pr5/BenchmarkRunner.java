16d28fec023b35549135551390bca6c5a6feb9b1
Max <maxim.chevychelov@gmail.com>
2023-12-15 22:36:10 +0500

    pr5

---
+++ src/test/java/edu/pr5/BenchmarkRunner.java
@@ -0,0 +1,17 @@
+package edu.pr5;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class BenchmarkRunner {
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(StudentBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(options).run();
+    }
+}
